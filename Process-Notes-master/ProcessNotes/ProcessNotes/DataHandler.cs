c6c9d15d61aae358714b23ed47bd6d064dbb8bf8
Daniel Magyar <44492760+YungShagger@users.noreply.github.com>
2019-10-03 13:05:43 +0200

    Add files via upload
    Exit Function works. Added and changed texts.

---
+++ Process-Notes-master/ProcessNotes/ProcessNotes/DataHandler.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProcessNotes
+{
+    public sealed class DataHandler
+    {
+
+
+        private static readonly object locker = new object();
+        private static DataHandler instance = null;
+        private int selectedIndex;
+        private Process[] processList;
+
+        public Process[] ProcessList
+        {
+            get
+            {
+                Process[] copy = new Process[processList.Length];
+                Array.Copy(processList, copy, processList.Length);
+                return copy;
+            }
+            private set { }
+        }
+        private DataHandler()
+        {
+            processList = Process.GetProcesses();
+        }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                return this.selectedIndex;
+            }
+            set
+            {
+                this.selectedIndex = value;
+            }
+        }
+
+        Dictionary<int, string> Comments = new Dictionary<int, string>();
+
+        public static DataHandler Instance
+        {
+            get
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DataHandler();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public string Get(int key)
+        {
+            string result = "";
+
+            if (Comments.ContainsKey(key))
+            {
+                result = Comments[key];
+            }
+
+            return result;
+        }
+
+
+        public void Set(int key, string value)
+        {
+            if (Comments.ContainsKey(key))
+            {
+                Comments[key] = value;
+            }
+            else
+            {
+                Comments.Add(key, value);
+            }
+        }
+
+        
+
+        public bool ContainsID(int ID)
+        {
+            return Comments.Keys.Contains(ID);
+        }
+    }
+}
+    
---
+++ Process-Notes-master/ProcessNotes/ProcessNotes/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessNotes
+{
+    public partial class CommentChanger : Form
+    {
+        DataHandler currentData = DataHandler.Instance;
+        public CommentChanger()
+        {
+            InitializeComponent();
+            int processIndex = currentData.ProcessList[currentData.SelectedIndex].Id;
+            textBoxComment.Text = currentData.Get(processIndex);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            string comment = textBoxComment.Text;
+            int processIndex = currentData.ProcessList[currentData.SelectedIndex].Id;
+            currentData.Set(processIndex, comment);
+            CommentChanger.ActiveForm.Hide();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBoxComment.Text = String.Empty;
+            CommentChanger.ActiveForm.Hide();
+        }
+    }
+}
